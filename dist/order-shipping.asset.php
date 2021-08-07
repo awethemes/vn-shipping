370f36fcbadd85a0e0583ddf6837d5552178e7d4
Van Anh <anhskohbo@gmail.com>
2021-08-07 22:35:20 +0700

    > npm run prod

--- dist/order-shipping.asset.php
+++ dist/order-shipping.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'wp-api-fetch', 'wp-date', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'e66072495ae1bffbccb68b0c53bee10c');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'wp-api-fetch', 'wp-date', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '481276289236dbfa8170318e904d87e9');
